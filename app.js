a35ce9af404d186a504f4ec2ee9e5738d922ecd3
DarGrz <d.grzegorczyk@outlook.com>
2022-06-14 11:31:27 +0200

    adding to todo list functions

--- app.js
+++ app.js
@@ -17,10 +17,22 @@ function addTodo(e) {
     todoDiv.classList.add('todo');
     // Create Li
     const newTodo = document.createElement('li');
-    newTodo.innerText ='hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     // Check mark button
     const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // Trash  button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // Append to list
+    todoList.appendChild(todoDiv);
+    //Clear Input text
+    todoInput.value = '';
 
 };
